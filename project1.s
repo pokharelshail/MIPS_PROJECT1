33a5a6dfdd5194b7be6d64968201b55a5802b283
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2020-10-15 21:22:43 -0400

    Print input message prompt

--- project1.s
+++ project1.s
@@ -1,5 +1,13 @@
+#base 28 a-r
 .data
 inputText:.asciiz "Input: "
 base28num:.space 11
 space:.asciiz "\n"
 outputText: .asciiz
+.text
+main:	
+        #print input prompt Input: 
+	li $s0, 0 
+	li $v0, 4
+	la $a0, inputText
+	
