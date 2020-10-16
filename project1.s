a30b182df9926b2bd26fedd869defb02bb48ec3c
pokharelshail <shreeshail.pokharel@bison.howard.edu>
2020-10-15 21:25:01 -0400

    Take input from user

--- project1.s
+++ project1.s
@@ -6,8 +6,16 @@ space:.asciiz "\n"
 outputText: .asciiz
 .text
 main:	
-        #print input prompt Input: 
+	#print input prompt Input: 
 	li $s0, 0 
 	li $v0, 4
 	la $a0, inputText
+	syscall
+	
+	li $v0, 8						
+	la $a0, base28num
+	li $a1, 11
+	syscall
+
+
 	
